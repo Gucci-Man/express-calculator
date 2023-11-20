bd8637baba02a12b0385d80c7d03e69f6a39ca48
mrtomno <adel.ngo@yahoo.com>
2023-11-19 20:51:16 -0600

    complete mode

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
+/********************** Mean **********************/
 app.get('/mean', function(req, res, next) {
     try{
         const {nums} = req.query
@@ -16,7 +17,7 @@ app.get('/mean', function(req, res, next) {
         if (!nums) {
             throw new ExpressError("nums are required.", 400)
         };
-        // split string then convert to int
+        // Split string then convert to int
         const numsArr = nums.split(',')
         
         for(let num of numsArr) {
@@ -29,6 +30,7 @@ app.get('/mean', function(req, res, next) {
         for(let i=0; i<intArr.length; i++) {
             total += intArr[i];
         }
+        // Calculate mean
         mean = total/intArr.length
         console.log(`mean is ${mean}`)
         
@@ -38,15 +40,16 @@ app.get('/mean', function(req, res, next) {
     }
 });
 
+/********************** Median **********************/
 app.get('/median', function(req, res, next) {
     try{
         const {nums} = req.query
-        const intArr = []; // will contain the integer converted numbers
+        const intArr = []; 
 
         if (!nums) {
             throw new ExpressError("nums are required.", 400)
         };
-        // split string then convert to int
+        // Split string then convert to int
         const numsArr = nums.split(',')
         
         for(let num of numsArr) {
@@ -77,12 +80,61 @@ app.get('/median', function(req, res, next) {
     }
 });
 
+/********************** Mode *************************/
+app.get('/mode', function(req, res, next) {
+    try{
+        const {nums} = req.query
+        const intArr = []; 
+        const freqTable = {} // Frequency table
+
+        if (!nums) {
+            throw new ExpressError("nums are required.", 400)
+        };
+        // Split string then convert to int
+        const numsArr = nums.split(',')
+        
+        for(let num of numsArr) {
+            if(!(parseInt(num))) {
+                throw new ExpressError(`${num} is not a number`, 400)
+            };
+            intArr.push(parseInt(num));
+        }
+
+        // Iterate through the array and update the frequency table
+        for(let num of intArr) {
+            if (freqTable[num] === undefined) {
+                freqTable[num] = 1;
+            } else {
+                freqTable[num]++;
+            }
+        }
+
+        // Identify the mode
+        let modeVal = null;
+        let highFreq = 0;
+
+        for (let num in freqTable) {
+            const frequency = freqTable[num];
+            if (frequency > highFreq) {
+                modeVal = num;
+                highFreq = frequency;
+            }
+        }
+
+        return res.status(200).json({operation: "mode", value: modeVal});
+
+    } catch (e) {
+        next(e);
+    }
+});
+
 // If page is not found, throw error
 app.use((req, res, next) => {
     const e = new ExpressError("Page Not Found", 404);
     next(e);
 })
 
+// Error-handler
 app.use(function (err, req, res, next) {
     let status = err.status || 500;
     let message = err.msg;
