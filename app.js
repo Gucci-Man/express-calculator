096043894681f7df49427714795b8deba076e0ac
mrtomno <adel.ngo@yahoo.com>
2023-11-19 18:30:40 -0600

    complete mean

--- app.js
+++ app.js
@@ -6,7 +6,21 @@ app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 app.get('/mean', function(req, res) {
-    res.send('<h1>This is the mean of numbers</h1>')
+    const {nums} = req.query
+    let mean=null;
+    let total=null;
+
+    // split string then convert to int
+    const numsArr = nums.split(',')
+    const intArr = numsArr.map(num => parseInt(num));
+    for(let i=0; i<intArr.length; i++) {
+        console.log(`num is ${intArr[i]}`);
+        total += intArr[i];
+    }
+    mean = total/intArr.length
+    console.log(`mean is ${mean}`)
+    
+    return res.json({operation: "mean", value: mean});
 })
 
 app.listen(3000, () => {
