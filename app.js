0369f4347c1c329f299faa3680d7112fb313a0c1
mrtomno <adel.ngo@yahoo.com>
2023-11-19 19:19:46 -0600

    added error handling

--- app.js
+++ app.js
@@ -1,27 +1,57 @@
 const express = require('express')
+const ExpressError = require('./expressError')
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.get('/mean', function(req, res) {
-    const {nums} = req.query
-    let mean=null;
-    let total=null;
+app.get('/mean', function(req, res, next) {
+    try{
+        const {nums} = req.query
+        let mean=null;
+        let total=null;
+        const intArr = []; // will contain the integer converted numbers
 
-    // split string then convert to int
-    const numsArr = nums.split(',')
-    const intArr = numsArr.map(num => parseInt(num));
-    for(let i=0; i<intArr.length; i++) {
-        console.log(`num is ${intArr[i]}`);
-        total += intArr[i];
+        if (!nums) {
+            throw new ExpressError("nums are required.", 400)
+        };
+        // split string then convert to int
+        const numsArr = nums.split(',')
+        
+        for(let num of numsArr) {
+            if(!(parseInt(num))) {
+                throw new ExpressError(`${num} is not a number`, 400)
+            };
+            intArr.push(parseInt(num));
+        }
+
+        for(let i=0; i<intArr.length; i++) {
+            total += intArr[i];
+        }
+        mean = total/intArr.length
+        console.log(`mean is ${mean}`)
+        
+        return res.status(200).json({operation: "mean", value: mean});
+    } catch (e) {
+        next(e);
     }
-    mean = total/intArr.length
-    console.log(`mean is ${mean}`)
-    
-    return res.json({operation: "mean", value: mean});
 })
+
+// If page is not found, throw error
+app.use((req, res, next) => {
+    const e = new ExpressError("Page Not Found", 404);
+    next(e);
+})
+
+app.use(function (err, req, res, next) {
+    let status = err.status || 500;
+    let message = err.msg;
+
+    return res.status(status).json({
+        error: {message, status}
+    });
+});
 
 app.listen(3000, () => {
     console.log('App running on port 3000')
